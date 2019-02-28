a0512ceae72d5698a595f2a1bdf2e43123038e93
Maddie <madalina.khaji10@gmail.com>
2019-02-28 15:33:51 +0100

    Implemented word edit

--- angular-client/src/app/components/word-crud/word-edit/word-edit.component.ts
+++ angular-client/src/app/components/word-crud/word-edit/word-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { WordsService } from '../../../services/words.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-word-edit',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WordEditComponent implements OnInit {
 
-  constructor() { }
+  wordForm: FormGroup;
+  _id: Number;
+  text: String = '';
+  lang: String = '';
+  category: String = '';
+  translation: String = 'No';
+  description: String = 'None';
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private wordsService: WordsService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.getWord(this.route.snapshot.params['id']);
+    this.wordForm = this.formBuilder.group({
+      'text' : [null, Validators.required],
+      'lang' : [null, Validators.required],
+      'category' : [null, Validators.required],
+      'translation' : [null],
+      'description' : [null]
+    });
   }
 
+  getWord(id) {
+    this.wordsService.getWord(id)
+      .subscribe(data => {
+        this._id = data._id;
+        this.wordForm.setValue({
+          text: data.text,
+          lang: data.lang,
+          category: data.category,
+          translation: data.translation,
+          description: data.description
+        });
+      });
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.wordsService.updateWord(this._id, form)
+      .subscribe(res => {
+        let id = res['_id'];
+        this.router.navigate(['/word-detail', id]);
+      }, (err) => {
+        console.log(err);
+      });
+  }
 }
